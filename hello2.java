0f28dbd272f220d2f1fea1cade1b94a9e8cf8ee8
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-03-05 16:43:29 +0530

    oops

---
+++ hello2.java
@@ -0,0 +1,17 @@
+ class Animal {
+    String color;
+     int age;
+
+     void initObj(String c, int a){
+        color=c;
+        age=a;
+     }
+     void display(){
+        System.out.println(color+" "+age);
+     }
+     public static void main(String[] args) {
+        Animal buzo = new Animal();
+        buzo.initObj("black",15);
+        buzo.display();
+     }
+}
