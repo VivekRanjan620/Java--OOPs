f4a09039317431e68d76087dc93f962b0b27d571
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-03-05 23:36:27 +0530

    Single-Inheritance

---
+++ Inheritance.java
@@ -0,0 +1,11 @@
+//  class Animal {
+//     void eat(){
+//         System.out.println("I am eating");
+//     }
+// }
+// class Dog extends Animal{
+//     public static void main(String[] args) {
+//         Dog d = new Dog();
+//         d.eat();
+//     }
+// }
--- hello2.java
+++ hello2.java
@@ -1,17 +1,17 @@
- class Animal {
-    String color;
-     int age;
+//  class Animal {
+//     String color;
+//      int age;
 
-     void initObj(String c, int a){
-        color=c;
-        age=a;
-     }
-     void display(){
-        System.out.println(color+" "+age);
-     }
-     public static void main(String[] args) {
-        Animal buzo = new Animal();
-        buzo.initObj("black",15);
-        buzo.display();
-     }
-}
+//      void initObj(String c, int a){
+//         color=c;
+//         age=a;
+//      }
+//      void display(){
+//         System.out.println(color+" "+age);
+//      }
+//      public static void main(String[] args) {
+//         Animal buzo = new Animal();
+//         buzo.initObj("black",15);
+//         buzo.display();
+//      }
+// }
