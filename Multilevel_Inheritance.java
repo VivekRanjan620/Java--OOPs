d966511d2ed30e254f6fb91a6b1b41a50a743af7
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-03-07 23:11:23 +0530

    Polymorphism

--- Multilevel_Inheritance.java
+++ Multilevel_Inheritance.java
@@ -1,33 +1,33 @@
-class A {
+// class A {
 
-    void showA() {
-        System.out.println("a class method");
-    }
-}
+//     void showA() {
+//         System.out.println("a class method");
+//     }
+// }
 
 
-class B extends A {
-    void showB() {
-        System.out.println("b class method");
-    }
-}
+// class B extends A {
+//     void showB() {
+//         System.out.println("b class method");
+//     }
+// }
 
-class C extends B {
-    void showC() {
-        System.out.println("c class method");
-    }
+// class C extends B {
+//     void showC() {
+//         System.out.println("c class method");
+//     }
 
-    public static void main(String[] args) {
-        A obj1 = new A();
-        obj1.showA();
-        System.out.println("-------------------");
-        B obj2 = new B();
-        obj2.showA();
-        obj2.showB();
-        System.out.println("-------------------");
-        C obj3 = new C();
-        obj3.showA();
-        obj3.showB();
-        obj3.showC();
-    }
-}
+//     public static void main(String[] args) {
+//         A obj1 = new A();
+//         obj1.showA();
+//         System.out.println("-------------------");
+//         B obj2 = new B();
+//         obj2.showA();
+//         obj2.showB();
+//         System.out.println("-------------------");
+//         C obj3 = new C();
+//         obj3.showA();
+//         obj3.showB();
+//         obj3.showC();
+//     }
+// }
--- hello.java
+++ hello.java
@@ -1,17 +1,17 @@
-class Animal {
-    public void eat() {
-        System.out.println("I am eating");
-    }
+// class Animal {
+//     public void eat() {
+//         System.out.println("I am eating");
+//     }
 
-    public static void main(String[] args) {
-        System.out.println("hii");
-        Animal buzo = new Animal();
-        buzo.eat();
-        buzo.run();
+//     public static void main(String[] args) {
+//         System.out.println("hii");
+//         Animal buzo = new Animal();
+//         buzo.eat();
+//         buzo.run();
 
-    }
+//     }
 
-    public void run() {
-        System.out.println("Buzo running");
-    }
-}
+//     public void run() {
+//         System.out.println("Buzo running");
+//     }
+// }
