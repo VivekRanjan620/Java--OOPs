9dd7fc63b495e416d9d3027152d69876c2f44cfd
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-03-08 22:39:14 +0530

    Update

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,28 +1,28 @@
 class Animal {
-    public void animalSound() {
-      System.out.println("The animal makes a sound");
-    }
+  public void animalSound() {
+    System.out.println("The animal makes a sound");
   }
-  
-  class Pig extends Animal {
-    public void animalSound() {
-      System.out.println("The pig says: wee wee");
-    }
+}
+
+class Pig extends Animal {
+  public void animalSound() {
+    System.out.println("The pig says: wee wee");
   }
-  
-  class Dog extends Animal {
-    public void animalSound() {
-      System.out.println("The dog says: bow wow");
-    }
+}
+
+class Dog extends Animal {
+  public void animalSound() {
+    System.out.println("The dog says: bow wows");
   }
-  
-  class Main {
-    public static void main(String[] args) {
-      Animal myAnimal = new Animal();  // Create a Animal object
-      Animal myPig = new Pig();  // Create a Pig object
-      Animal myDog = new Dog();  // Create a Dog object
-      myAnimal.animalSound();
-      myPig.animalSound();
-      myDog.animalSound();
-    }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Animal myAnimal = new Animal(); // Create a Animal object
+    Animal myPig = new Pig(); // Create a Pig object
+    Animal myDog = new Dog(); // Create a Dog object
+    myAnimal.animalSound();
+    myPig.animalSound();
+    myDog.animalSound();
   }
+}
