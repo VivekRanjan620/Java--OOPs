7d1cf1164f26b419a485598384d0ba8d41c5c41f
VivekRanjan620 <143477763+VivekRanjan620@users.noreply.github.com>
2024-03-10 21:48:58 +0530

    update

--- Single_Inheritance.java
+++ Single_Inheritance.java
@@ -1,21 +1,21 @@
-//  class A {
+ class A {
    
-//    void showA()
-//     {
-//         System.out.println("a class method");
-//     }
-// }
-// class B extends A
-// {
-//     void showB(){
-//         System.out.println("b class method");
-//     }
-//     public static void main(String[] args) {
-//         A obj1=new A();
-//         obj1.showA();
-//         System.out.println("-------------------");
-//         B obj2=new B();
-//         obj2.showA();
-//         obj2.showB();
-//     }
-// }
+   void showA()
+    {
+        System.out.println("a class method");
+    }
+}
+class B extends A
+{
+    void showB(){
+        System.out.println("b class method");
+    }
+    public static void main(String[] args) {
+        A obj1=new A();
+        obj1.showA();
+        System.out.println("-------------------");
+        B obj2=new B();
+        obj2.showA();
+        obj2.showB();
+    }
+}
